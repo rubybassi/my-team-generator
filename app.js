e17a02839f5e3aedc9c486ef405caeab3c0ac976
Sandeep Bassi <thecodingfiles@gmail.com>
2020-11-29 02:19:00 +0000

    added promises to fs so can use writeFileSync and catch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -58,7 +58,7 @@ const initialPrompt = () => {
     .then((answers) => {
       if (answers.role === "Manager") {
         // console.log('manager is,', answers);
-        const manager = new Manager(answers.name,answers.id,answers.email,answers.officeNumber);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
         team.push(manager);
         // console.log('this is teams', team);
         additionalPrompt();
@@ -72,9 +72,9 @@ const initialPrompt = () => {
         additionalPrompt();
       }
     });
-  // function for next member. If yes, call intialPrompt. Else render and write html file
 };
 
+  // function for next member. If yes, call intialPrompt. Else render and write html file
 const additionalPrompt = () => {
   inquirer
     .prompt([
@@ -89,9 +89,16 @@ const additionalPrompt = () => {
         initialPrompt();
       } else {
         console.log('team', team);
+        const html = render(team);
+        fs.writeFileSync('./output/team.html', html)
+        .then(() => {
+         console.log('team file generated');
+        }).catch((err) => {
+        //  console.log(err);
+        });
       }
     }); 
   };
 
 initialPrompt();
-//need loop for add another (call intial prompt)
+
